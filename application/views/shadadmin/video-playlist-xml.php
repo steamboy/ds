3a18db268f9be75d94df53f150c4164bfaae2b82
Gino <gino.carlo.cortez@gmail.com>
2010-11-02 09:44:59 +0800

    file path change for OSX; absoulute path applied

--- application/views/shadadmin/video-playlist-xml.php
+++ application/views/shadadmin/video-playlist-xml.php
@@ -7,7 +7,7 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
     <?php
     foreach ($playlist_content as $playlist_contents){
     ?>
-        <item count_id="<?php echo $playlist_contents->sort_id;?>" id="<?php echo $playlist_contents->id;?>" name="<?php echo $playlist_contents->name;?>" type="flv" file="content/video/<?php echo $playlist_contents->filename;?>" display="<?php echo $playlist_contents->display;?>"/>
+        <item count_id="<?php echo $playlist_contents->sort_id;?>" id="<?php echo $playlist_contents->id;?>" name="<?php echo $playlist_contents->name;?>" type="flv" file="file:///DS/content/video/<?php echo $playlist_contents->filename;?>" display="<?php echo $playlist_contents->display;?>"/>
     <?php } ?>
     </items>
 </data>
